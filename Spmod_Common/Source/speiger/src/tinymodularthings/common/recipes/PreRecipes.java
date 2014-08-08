4bee1f90c7f668561ac01214e06961543c9cbd96
Speiger <Speiger@privat>
2014-08-08 20:31:57 +0200

    -Adding Recipes for the NetherCrystal

--- Spmod_Common/Source/speiger/src/tinymodularthings/common/recipes/PreRecipes.java
+++ Spmod_Common/Source/speiger/src/tinymodularthings/common/recipes/PreRecipes.java
@@ -11,6 +11,7 @@ import speiger.src.spmodapi.common.util.proxy.PathProxy;
 import speiger.src.tinymodularthings.common.config.ModObjects.TinyBlocks;
 import speiger.src.tinymodularthings.common.config.ModObjects.TinyItems;
 import speiger.src.tinymodularthings.common.enums.EnumIngots;
+import speiger.src.tinymodularthings.common.items.tools.ItemNetherCrystal;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class PreRecipes
@@ -21,5 +22,8 @@ public class PreRecipes
 		GameRegistry.addShapelessRecipe(new ItemStack(Item.skull, 1, 1), new Object[]{Item.diamond, Item.diamond, Block.coalBlock, Item.arrow, Item.arrow, Item.bone, Item.bone});
 		
 		GameRegistry.addRecipe(new ItemStack(TinyItems.interfaceBlock, 4, 0), new Object[]{"XYX", "CVC", "XBX", 'B', TinyBlocks.normalPipe, 'Y', Block.hopperBlock, 'C', ItemGear.getGearFromType(GearType.Redstone), 'V', Block.chest, 'X', new ItemStack(TinyItems.ingots, 1, EnumIngots.Aluminum.getIngotMeta())});
+	
+		GameRegistry.addRecipe(ItemNetherCrystal.createEmptyNetherCrystal(TinyItems.netherCrystal.itemID), new Object[]{"XYX", "YCY", "XYX"}, 'X', Item.netherStar, 'C', Item.diamond, 'Y', Item.enderPearl);
+		GameRegistry.addShapelessRecipe(ItemNetherCrystal.createEmptyNetherCrystal(TinyItems.netherCrystal.itemID), new Object[]{new ItemStack(TinyItems.netherCrystal, 1, 5), Item.diamond, Item.enderPearl});
 	}
 }
